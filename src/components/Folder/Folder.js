3cb9875fa1a84b4df0b4c494208571f2d7381f09
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-09 22:50:43 -0800

    Add folder expansion/collapse functionality

--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 
+import FolderContent from '../FolderContent';
+
 import { fileStatus } from '../../constants';
-const { COLLAPSED, EXPANDEND } = fileStatus;
+const { COLLAPSED, EXPANDED } = fileStatus;
 
 export default class Folder extends React.Component {
   state = {
@@ -9,10 +11,18 @@ export default class Folder extends React.Component {
   };
 
   render() {
+    const { folder } = this.props;
+    const { status } = this.state;
     return (
       <li>
-        <div>{ this.props.folder.name }</div>
+        <div onClick={ this.toggleFolder }>{ this.props.folder.name }</div>
+        { status === EXPANDED && <FolderContent content={ folder } /> }
       </li>
     );
   }
+
+  toggleFolder = () => {
+    const newStatus = this.state.status === COLLAPSED ? EXPANDED : COLLAPSED;
+    this.setState({ status: newStatus });
+  };
 }
