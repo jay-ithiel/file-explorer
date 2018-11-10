22ce467b55d8c14aa8bdadb6d788b3106df8af99
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-09 22:54:15 -0800

    Add Icons

--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -13,9 +13,16 @@ export default class Folder extends React.Component {
   render() {
     const { folder } = this.props;
     const { status } = this.state;
+    const isPrivateFolder = folder.private;
     return (
       <li>
-        <div onClick={ this.toggleFolder }>{ this.props.folder.name }</div>
+        <div onClick={ this.toggleFolder }>
+          { status === COLLAPSED && <ExpandIcon /> }
+          { status === EXPANDED && <CollapseIcon /> }
+          { isPrivateFolder && <PrivateFolderIcon /> }
+          { !isPrivateFolder && <FolderIcon /> }
+          <h3>{this.props.folder.name}</h3>
+        </div>
         { status === EXPANDED && <FolderContent content={ folder } /> }
       </li>
     );
@@ -26,3 +33,22 @@ export default class Folder extends React.Component {
     this.setState({ status: newStatus });
   };
 }
+
+
+/** Helper Components */
+
+const CollapseIcon = () => (
+  <img alt="collapse" src="https://res.cloudinary.com/ddgtwtbre/image/upload/v1541825178/collapse_j5f9sj.png" />
+);
+
+const ExpandIcon = () => (
+  <img alt="expand" src="https://res.cloudinary.com/ddgtwtbre/image/upload/v1541825178/expand_w5m2es.png" />
+);
+
+const FolderIcon = () => (
+  <img alt="folder" src="https://res.cloudinary.com/ddgtwtbre/image/upload/v1541824963/folder_tazuxm.png" />
+);
+
+const PrivateFolderIcon = () => (
+  <img alt="private-folder" src="https://res.cloudinary.com/ddgtwtbre/image/upload/v1541824963/private_wjyyyc.png" />
+);
