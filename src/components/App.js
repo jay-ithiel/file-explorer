c4d3f239cedcd1f56075a0035b74ae77b8506737
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-09 22:01:43 -0800

    Import & render Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../assets/stylesheets/App.css';
 
+import Header from './Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header/>
       </div>
     );
   }
