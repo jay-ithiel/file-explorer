1f831960aaec2c559dc743a16aabe10b4ffed9f6
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-09 23:12:51 -0800

    Create App.context and wrap App with AppProvider

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import '../assets/stylesheets/App.css';
 
+import { AppProvider } from './App.context';
+
 import Header from './Header';
 import RootFolder from './RootFolder';
 import Footer from './Footer';
 
 class App extends Component {
+  state = {
+    activeId: '',
+  };
+  
   render() {
+    const contextValue = {
+      data: this.state,
+      updateActiveId: this.updateActiveId,
+    };
+    
     return (
       <div className="App">
-        <Header/>
-        <RootFolder/>
-        <Footer/>
+        <AppProvider value={ contextValue }>
+          <Header/>
+          <RootFolder/>
+          <Footer/>
+        </AppProvider>
       </div>
     );
   }
+
+  updateActiveId = activeId => this.setState({ activeId });
 }
 
 export default App;
