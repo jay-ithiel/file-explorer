5c5eae7ecae1ac28aa5a2b0ca0734ae927e67e95
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-10 00:08:40 -0800

    add comment

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,19 @@ import RootFolder from './RootFolder';
 import Footer from './Footer';
 
 class App extends Component {
+  /**
+   * Each file/folder has a unique 'id'. When clicked,
+   * updateActiveId will be invoked, setting state.activeId
+   * to the id of the file/folder that was clicked.
+   * 
+   * On the file/folder level, if the activeId matches its
+   * id, it will be toggled as 'active', giving it the
+   * 'active' styles
+   * 
+   * Can deploy this logic to keep track of the state of the 
+   * file explorer for the Advanced 'Export' feature
+   * 
+   * */
   state = {
     activeId: '',
   };
