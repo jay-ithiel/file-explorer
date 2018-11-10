3d16ecf16e1fb0cd0cea4affa0565c188862883c
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-09 22:12:55 -0800

    Import & render Footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import '../assets/stylesheets/App.css';
 
 import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Footer/>
       </div>
     );
   }
