900e751b9172ad82c172ea950a463baa0cfee7f4
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-09 21:56:57 -0800

    Initial setup Remove boilerplate Restructure directories Update import paths

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../assets/stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
