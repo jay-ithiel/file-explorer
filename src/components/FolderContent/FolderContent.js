5dd98d1d7829aa01c7eb851493be4aa609f04c78
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-09 22:41:20 -0800

    map childrenWithKEys

--- src/components/FolderContent/FolderContent.js
+++ src/components/FolderContent/FolderContent.js
@@ -1,7 +1,19 @@
 import * as React from 'react';
 
+
+
 const FolderContent = ({ content }) => {
   const { children } = content;
+  if (!children) return <ul></ul>;
+
+  /**
+   * Map childrenWithKeys for because we need non-changing
+   * unique values for React children elements
+   * */
+  const childrenWithKeys = children.map((child, index) => (
+    { ...child, key: index }
+  ));
+
   return (
     <ul>
       
