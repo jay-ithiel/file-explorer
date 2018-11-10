13c99d48f02a25bc1965f0c53a960c8788ccdd14
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-09 22:55:54 -0800

    Add icon

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -2,8 +2,18 @@ import * as React from 'react';
 
 const File = ({ file }) => (
   <li>
-    <h3>{ file.name }</h3>
+    <div>
+      <FileIcon />
+      <h3>{ file.name }</h3>
+    </div>
   </li>  
 );
 
 export default File;
+
+
+/** Helper Components */
+
+const FileIcon = () => (
+  <img alt="file" src="https://res.cloudinary.com/ddgtwtbre/image/upload/v1541824710/file_krfecv.png" />
+);
