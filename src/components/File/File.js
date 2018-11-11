e48384545af517dd34798505fcf0562cdfd924af
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-10 21:39:17 -0800

    Refactor File component Refactor into functional component Remove unnecessary code Abstract Icon component out

--- src/components/File/File.js
+++ src/components/File/File.js
@@ -1,37 +1,24 @@
 import * as React from 'react';
 
 import { AppConsumer } from '../App.context';
-import { generateToken } from '../../utils';
+import { FileIcon } from '../Icons';
 
-export default class File extends React.Component {
-  state = { id: generateToken() };
-  
-  render() {
-    const { id } = this.state;
-    return (
-      <li>
-        <AppConsumer>
-          {
-            ({ data, updateActiveId }) => {
-              const activeClass = data.activeId === id ? 'active' : '';
-              const handleClick = () => updateActiveId(id);
-              return (
-                <div className={ activeClass } onClick={ handleClick }>
-                  <FileIcon />
-                  <h3>{ this.props.file.name }</h3>
-                </div>
-              );
-            }
-          }
-        </AppConsumer>
-      </li>
-    );
-  }
-}
-
-
-/** Helper Components */
-
-const FileIcon = () => (
-  <img alt="file" src="https://res.cloudinary.com/ddgtwtbre/image/upload/v1541824710/file_krfecv.png" />
+const File = ({ file, handleClick }) => (
+  <li>
+    <AppConsumer>
+      {
+        ({ activeId, updateActiveId }) => {
+          const activeClass = activeId === file.id ? 'active' : '';
+          return (
+            <div className={ activeClass } onClick={ handleClick }>
+              <FileIcon />
+              <h3>{ file.name }</h3>
+            </div>
+          );
+        }
+      }
+    </AppConsumer>
+  </li>
 );
+
+export default File;
