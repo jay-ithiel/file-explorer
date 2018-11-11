105aa5cd151c16803c4506db428dedff4e1c99a6
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-11 00:26:16 -0800

    add close icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,9 @@ const Header = () => (
   <header>
     <h1>Title</h1>
     <h3>Label</h3>
+    <div>
+      <img alt="Close icon" src="https://res.cloudinary.com/ddgtwtbre/image/upload/v1541924382/Screen_Shot_2018-11-11_at_12.11.57_AM_oljjky.png" />
+    </div>
   </header>
 );
 
